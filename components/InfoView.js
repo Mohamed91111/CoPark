f0bc2d907adce2d133edf9a625a4e8e98f938560
alenhorozic <alenhorozicrhotmail.com>
2021-03-04 13:50:49 +0100

    add Components and Views

---
+++ components/InfoView.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Text} from 'react-native';
+
+const InfoView = props => {
+    return (
+        <View style={styles.infoView}>
+           <Text> Create Parking Info</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    infoView: {
+       backgroundColor:'#ffffff',
+       display:'flex',
+       width:'100%',
+       height: '72%',
+       alignItems:'center',
+       justifyContent:'center',
+       borderBottomWidth:1,
+       borderBottomColor:'#757575'
+    }
+});
+
+export default InfoView;
