1a74f8ffd0e1eb2a8146b5445df427915b64dbcd
alenhorozic <alenhorozicrhotmail.com>
2021-03-16 11:23:46 +0100

    curentPosition on map

--- components/MapView.js
+++ components/MapView.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Component } from 'react';
-import MapView  from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import {ActivityIndicator} from 'react-native';
+import MapView, {PROVIDER_DEFAULT}  from 'react-native-maps';
 
+const initialState = {
+  latitude: null,
+  longitude: null,
+  latitudeDelta: 0.0050,
+  longitudeDelta: 0.0050 
+}
 
+const AppMap = ()=>  {  
+  const [curentPosition, setCurentPosition] = useState(initialState);  
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position=>{
+      //alert(JSON.stringify(position))
+      const {longitude,latitude} = position.coords;
+      setCurentPosition({
+        ...curentPosition,
+        latitude,
+        longitude,
+      })
+    },
+    error=>alert(error.message),
+    {timeout:3000,maximumAge:2000}
+    )
+    
+  }, [])
 
-export default class App extends Component {  
-render() {    
-  return (      
-  <MapView        
+  return curentPosition.latitude ? (      
+  <MapView  
+     provider= {PROVIDER_DEFAULT}      
      style={{flex: 1}}        
-     region={{ latitude: 57.705432, longitude: 11.972467, latitudeDelta: 0.0050, longitudeDelta: 0.0050 }}       
+     region={curentPosition }       
      showsUserLocation={true} 
      showsScale={true}
   />
-    );
-  }
-}
+    ) : <ActivityIndicator style={{flex:1}} animating size="large"/>
+};
+
+export default AppMap;
