fea1f2c8f24c8dabaa2b0ee5b833d0049a50ef83
alenhorozic <alenhorozicrhotmail.com>
2021-03-17 10:59:50 +0100

    Add icons in footer

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import { MainStackNavigator, ContactStackNavigator, InfoStackNavigator } from "./StackNavigator";
+
+import Icon from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'HomeMap') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'InfoApp'){
+          iconName = focused ? 'information-circle' : 'information-circle-outline';
+        } else if (route.name === 'Contact'){
+          iconName = focused ? 'at-circle' : 'at-circle-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={size}  color={color}/>;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+    }}
+    >
       <Tab.Screen name="HomeMap" component={MainStackNavigator} />
       <Tab.Screen name="InfoApp" component={InfoStackNavigator} />
       <Tab.Screen name="Contact" component={ContactStackNavigator} />
--- screens/ParkingInfo.js
+++ screens/ParkingInfo.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 
 const ParkingInfo = () => {
   return (
     <View style={styles.center}>
+     <AntDesign name="rightcircleo" size={24} color="green" />
       <Text>This is the ParkingInfo screen</Text>
     </View>
   );
