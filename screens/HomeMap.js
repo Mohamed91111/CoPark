521beec7918517fc08195a9fd812c77313b09162
alenhorozic <alenhorozicrhotmail.com>
2021-03-19 12:21:34 +0100

    map whi markers

--- screens/HomeMap.js
+++ screens/HomeMap.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, View } from "react-native";
 import MapView from "../components/MapView";
 
 const HomeMap = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
-    <MapView style={{ flex: 1 }} />
-    <View style={{
-            position: 'absolute',//use absolute position to show button on top of the map
-            top: '50%', //for center align
-            alignSelf: 'center' //for align to right
-        }}>
-      <Button
-        title="Go to ParkingInfo Screen"
-        onPress={() => navigation.navigate("ParkingInfo")} // We added an onPress event which would navigate to the ParkingInfo screen
-      />
-      </View>
+      <MapView style={{ flex: 1 }}>
+        <View
+          style={{
+            position: "absolute", //use absolute position to show button on top of the map
+            top: "50%", //for center align
+            alignSelf: "center", //for align to right
+          }}
+        >
+          <Button
+            title="Go to ParkingInfo Screen"
+            onPress={() => navigation.navigate("ParkingInfo")} // We added an onPress event which would navigate to the ParkingInfo screen
+          />
+        </View>
+      </MapView>
     </View>
   );
 };
